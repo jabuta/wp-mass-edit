c670c67e9ac6f62f99f1ff0949e68a998e8bd4b6
jabuta <lfalvaz@gmail.com>
2024-01-11 20:38:11 -0500

    Working wp cli bash script file maker. WP authentication working but too slow.

---
+++ authtest.py
@@ -0,0 +1,36 @@
+import requests
+import base64
+from access import keys
+
+# Define your WP URL
+url = 'https://www.bizlatinhub.com/wp-json/wp/v2'
+
+# Define WP Keys
+user = keys['wp_key']
+password = keys['user']
+
+# Create WP Connection
+wp_connection = user + ':' + password
+
+# Encode the connection of your website
+token = base64.b64encode(wp_connection.encode())
+
+# Prepare the header of our request
+headers = {
+    'Authorization': 'Basic ' + token.decode('utf-8')
+    }
+
+def test_authentication():
+    # Attempt to create a post without data
+    response = requests.post(url + '/posts', headers=headers)
+    
+    if response.status_code == 401 or response.status_code == 403:
+        print("Authentication Failed", response.status_code, response.text)
+    elif response.status_code == 400:
+        print("Authentication Successful")
+    else:
+        print("Unexpected response", response.status_code, response.text)
+
+
+
+test_authentication()
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 import base64
+from access import keys, url
 
-keys = {
-    'wp_key': 'XXXX XXXX XXXX XXXX XXXX XXXX',
-    'user': 'username'
-    }
+# keys = {
+#    'wp_key': 'XXXX XXXX XXXX XXXX XXXX XXXX',
+#    'user': 'username'
+#    }
 
-# Define your WP URL
-url = 'https://www.bizlatinhub.com/wp-json/wp/v2'
+
 
 # Define WP Keys
 user = keys['wp_key']
@@ -25,29 +25,72 @@ headers = {
     }
 
 
-links_file_path = "/home/felo/workspace/github.com/jabuta/wp-mass-edit/files/links_remove.txt"
+file_folder_location = "/home/felo/workspace/github.com/jabuta/wp-mass-edit/files"
 
 
-def load_links(links_file_path):
+def load_links(files_path):
     from urllib.parse import urlparse
-    links_remove = []
-    links_f = open(links_file_path, "r")
-    for link in links_f:
-        links_remove.append(urlparse(link.rstrip("\n"))[2].replace("/",""))
-    links_f.close()
-    print(links_remove)
-    return links_remove
+    import csv
+    redirect_list = []
+    with open(files_path + "/redirects.csv","r") as redirectfile:
+        redirects = csv.reader(redirectfile, delimiter="	",)
+        next(redirects, None)
+        for redirect in redirects:
+            from_url = urlparse(redirect[0]).path
+            to_url = urlparse(redirect[1]).path if redirect[1] != "DELETE" else "DELETE"
+            redirect_list.append([from_url,to_url])
+    return redirect_list
 
 
+def create_db_update_commands(redirect_list):
+    commands = []
+    for redirect in redirect_list:
+        if redirect[1] == "Homepage":
+            commands.append(f'wp search-replace "{redirect[0]}" "/" --precise --recurse-objects --all-tables-with-prefix --include-columns=post_content')
+        elif redirect[1] != "DELETE":
+            commands.append(f'wp search-replace "{redirect[0]}" "{redirect[1]}" --precise --recurse-objects --all-tables-with-prefix --include-columns=post_content')
+    print(commands)
+    return commands
 
-def post_id_from_slug(slugs):
-    slugids = []
-    for slug in slugs:
-        r = requests.get(url + f'/posts?slug={slug}&_fields=id')
-        slugids.append((slug,r.json()[0]['id']))
-        print((r.json()[0]['id'],slug))
-    return slugids
+def save_commands(commands, files_path):
+    with open(files_path + "/commands.txt", "a") as f:
+        f.write("\n".join(commands))
+    print("commands are ready")
 
-remove_links = load_links(links_file_path)
 
-print(post_id_from_slug(remove_links))
+links_list = load_links(file_folder_location)
+command_list = create_db_update_commands(links_list)
+
+def ids_to_delete(redirect_list):
+    ids_delete = []
+    for slug in redirect_list:
+        print(slug)
+        r = requests.get(url + f'/posts?slug={slug[0].replace("/","")}&_fields=id')
+        ids_delete.append((r.json()[0]['id']))
+    return ids_delete
+
+def unpublish_commands(ids_unpublish):
+    commands =[]
+    for id in ids_unpublish:
+        commands.append(f'wp post update {id} --post_status=draft')
+    return commands
+
+def delete_ids(ids_delete,headers,url):
+    args = {'status': 'draft'}
+    id_result = []
+    for id in ids_delete:
+        r = requests.post(url + "/posts/" + str(id), headers=headers, json=args)
+        print(r.json())
+        id_result.append((id,r.json()['code']))
+    return id_result
+
+def save_results(deleted_ids, files_path):
+    with open(files_path + "/deleted_ids.txt", "w") as f:
+        for deleted_id in deleted_ids:
+            f.write(f'{deleted_id[0]}, {deleted_id[1]}')
+            f.write("\n")
+    print("saved results")
+
+posts_to_delete = ids_to_delete(links_list)
+unpublish = unpublish_commands(posts_to_delete)
+save_commands(command_list + unpublish, file_folder_location)
